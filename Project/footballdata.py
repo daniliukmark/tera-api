e5c6f94ff6d0409092aa2a8c82c10cb4672933e5
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-08-16 01:31:26 +0300

    commit

--- Project/footballdata.py
+++ Project/footballdata.py
@@ -7,46 +7,83 @@ class FootballData:
     
     main_url = 'http://www.vilniausfutbolas.lt/lyga/III-lyga/20'
     
-
     def get_target_url(self):
         first_response = requests.get(self.main_url)
         if first_response.status_code != 200:
-            
-            return print('Request failed with status code:', first_response.status_code)
+            print('Request failed with status code:', first_response.status_code)
+            return
         soup = BeautifulSoup(first_response.content, 'html.parser')
         for link in soup.select('a[href*="turnyrine-lentel"]'):        
             return link['href']
-    
-    def format_data(self):
+
+    def get_table(self):
         target_url = self.get_target_url()
+        if target_url is None:
+            return
         final_response = requests.get(target_url)
         if final_response.status_code != 200:
-            return print('Final request failed with status code:', final_response.status_code)
+            print('Final request failed with status code:', final_response.status_code)
+            return
         soup = BeautifulSoup(final_response.content, 'html.parser')
-        target_table = soup.find('table', class_='standings')
+        table = soup.find('table', class_='standings')
+        if table:
+            return table
+        else: 
+            print("Table's location has changed")
+            return
+    
+    def format_data(self):
+        target_table = self.get_table()
+        if target_table is None:
+            return      
         headings = []
         rows = []
-        th_elements = target_table.find_all('th')
-        for th in th_elements:
-            headings.append(th.text)
+        
+        for th_elements in target_table.find_all('th'):
+            headings.append(th_elements.text)
         for tr_blocks in target_table.find_all('tr')[1:]:
             seperated_rows = []
             for td_blocks in tr_blocks.find_all('td'):
                 seperated_rows.append(td_blocks.text.strip())
+                a_tag = td_blocks.find('a')
+                if a_tag:
+                    img_tag = a_tag.find('img')
+                    if img_tag and img_tag.has_attr('src'):
+                        img_name = a_tag.get_text(strip=True).lower().replace(' ', '-')
+                        seperated_rows.append(img_name.strip())
+                    else: 
+                        seperated_rows.append(None)      
             rows.append(seperated_rows)
+        headings.insert(headings.index('Komanda') + 1, 'Logo')
         return headings, rows
 
+    #Create a list of images and save images in a distinct file
+    def get_logo(self):
+        target_table = self.get_table()
+        if target_table is None:
+            return 
+        target_url = self.get_target_url()
+        if target_url is None:
+            return
+        target_response = requests.get(target_url)
+        if target_response.status_code != 200:
+            return
+        for tr_blocks in target_table.find_all('tr')[1:]:
+            pass      
+
+
+
     def get_data(self):
-        try:
+        try: 
             headings, rows = self.format_data()
             my_lst = []
             for row in rows: 
-                combined = dict(list(zip(headings, row)))
+                combined = dict(zip(headings, row))
                 my_lst.append(combined)
             json_data = json.dumps(my_lst, ensure_ascii=False, indent=4)
             return json_data
         except:
-            return print("Response status is not 200")
+            return 
 
     def save_data(self):
         try:
@@ -54,5 +91,7 @@ class FootballData:
             with open('footballdata.json', 'w', encoding='utf-8') as json_file:
                 json.dump(json_string, json_file, ensure_ascii=False, indent=4)
         except:
-            return print('Json string cannot be serialized. Response status is not 200')
+            return print('Json string cannot be serialized. Go to the link and check whether the table is there')
             
+my_obj = FootballData()
+print(my_obj.get_data())
---
+++ Project/images.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+
+url = 'http://www.vilniausfutbolas.lt/turnyrine-lentele/20?comp_id=37'
+xresponse = requests.get(url)
+if xresponse.status_code != 200:
+    print('Final request failed with status code:', xresponse.status_code)
+    #return
+soup = BeautifulSoup(xresponse.content, 'html.parser')
+table = soup.find('table', class_='standings')
+
+if table: 
+    #img_related_tags = table.find_all('img')
+    for tr_block in table.find_all('tr'):
+        a_tag = tr_block.find('a')
+        if a_tag:
+            img_tag = a_tag.find('img')
+            if img_tag: #and img_tag.has.attr('src'):
+                img_src = a_tag.img['src']
+            #img_text = a_tag.get_text(strip=True).lower().replace(' ', '-')
+                print(img_src)
+#            
+# GET THE LINKS ONLY
+# # #    
+    #dict_images_names = [{img.text.lower(): img['src']} for img in img_related_tags]
+    #print(dict_images_names)
+    
+"""output_dir = "logo_images"
+os.makedirs(output_dir, exist_ok=True)
+
+for index, image_url in enumerate(image_urls, start=1):
+    response = requests.get(image_url)
+    
+    if response.status_code == 200:
+        image_name = f"image_{index}.jpg"  # Or use a different naming scheme
+        image_path = os.path.join(output_dir, image_name)
+        
+        with open(image_path, "wb") as f:
+            f.write(response.content)
+"""
